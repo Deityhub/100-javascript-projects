66a5f53e087baee2d9fd380ea833ca28b5908a49
Elyse Holladay Cherewaty <eholladay@indeed.com>
2017-09-28 15:25:34 -0500

    finally actually returning total properly when called recursively

--- js-learning/exercises/ex-4-recursion/index.js
+++ js-learning/exercises/ex-4-recursion/index.js
@@ -1,3 +1,4 @@
+const arrays = require('../../data/arraysOfArrays');
 /*
  * sum
  *
@@ -9,28 +10,34 @@
 
 // const foo = [ 1, [ 2, 3, 4, 5, [ 6, 7, 8 ] ] ]; // 36
 // const foo = [ 2, 5, [ 3, 4 ], 8 ]; // 22
-const foo = [ 2, 4, 5 ];
+// const foo = [ 2, 4, 5 ];
+const foo = [ 1, [ 8 ], 5 ];
 
 
 const sum = function(param) {
     return param.reduce(function(total, elem) {
-        console.log("total is " + total);
-        console.log("elem is " + elem + " — isArray(elem) is " + Array.isArray(elem));
+        // console.log("total is " + total);
+        // console.log("elem is " + elem + " — isArray(elem) is " + Array.isArray(elem));
 
         // check to see if elem (the item being iterated over) is an array
         if (Array.isArray(elem) === false) {
             total += elem;
-            console.log("now the total is " + total);
+            // console.log("now the total is " + total);
             // if it IS NOT: add it to the accumulator (total)
             // if it IS: call function recursively to see if elem is an array...
         } else {
-            console.log("i must be an array");
-            sum(elem);
+            // console.log("i must be an array");
+            total += sum(elem);
         }
         return total;
     }, 0);
 }
 
-console.log("test foo should be 11, is: " + sum(foo) ); // 11
+// console.log(sum(arrays) ); // 36
+// console.log("test arrays[0]: should be 36, is: " + sum(arrays[0]) ); // 36
+// console.log("test arrays[1]: should be 72, is: " +  sum(arrays[1]) ); // 72
+// console.log("test arrays[2]: should be 1, is: " +  sum(arrays[2]) ); // 1
+// console.log("test arrays[3]: should be 3, is: " +  sum(arrays[3]) ); // 3
+
 
 module.exports = sum;
