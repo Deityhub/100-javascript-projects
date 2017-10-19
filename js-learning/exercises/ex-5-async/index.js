214c20c51ffff97005f174335322a26b2914cf44
Elyse Holladay Cherewaty <eholladay@indeed.com>
2017-10-19 13:31:47 -0500

    edits and notes from pairing with ashley

--- js-learning/exercises/ex-5-async/index.js
+++ js-learning/exercises/ex-5-async/index.js
@@ -14,33 +14,17 @@ const counter = function (object) {
     // set num to value of start from obj
     // add 1 to num until it is = to end
     var num = object.start;
+    console.log(num);
     const increment = function() {
         num += 1;
-        onIncrement(num);
+        console.log(num);
+        // onIncrement(num);
         // call whatever as many times as start-end recursively
         // if num <= end
-        increment(num);
+        // increment(num);
     };
-    setTimeout(increment, object.delay); // every 100ms
+    // setTimeout(increment, object.delay); // every 100ms
     object.callback();
 }
 
 module.exports = counter;
-
-
-// user clicked
-// doing things
-fetch('/random-url', () => {
-    // handle response
-})
-
-// I don't have access to the response because it's not back yet
-
-// console.log(num);
-// while (num <= object.end) {
-//     // after the delay, increment num+=1
-//     // and call onInc(num)
-//     num += 1;
-//     object.onIncrement(num);
-// }
-//     // settimeout with a delay of 100
