987950fd03f2cc413179f766e4113badd8db31a9
Elyse Holladay Cherewaty <eholladay@indeed.com>
2017-12-06 15:31:06 -0800

    parsing

--- js-learning/exercises/ex-6-css-parsing/index.js
+++ js-learning/exercises/ex-6-css-parsing/index.js
@@ -16,12 +16,33 @@ const parseCSS = function(path) {
         // create here an array of rules that use vendor prefixes
         // so we need to pass `data` to the rework css parser
         // and then find properties that start with a dash?
+
+        // this fails with
+        // return this.mapVisit(node.stylesheet.rules, '\n\n');
+        // TypeError: Cannot read property 'rules' of undefined
+        // var result = css.stringify(data);
+        
+        
+        
+        
+        // these two lines as written fail with
+        //     if ('/' != css.charAt(0) || '*' != css.charAt(1)) return;
+        // TypeError: css.charAt is not a function
+        // var obj = css.parse(data);
+        // console.log(css.stringify(obj));
+        // as does this one:
+        // var result = css.parse(data, { source: './test.css' });
+        // because data isn't yet a css file?
+        var cssdata = data.toString();
+        // console.log(cssdata);
+        var result = css.parse(cssdata, { source: './test.css' });
+        console.log(result); // there we go
     });
 }
 
 // TODO
 // "full path to a CSS file" - is there a way to enforce or require absolute paths, or does a relative path count?
-// parseCSS('./test.css');
+parseCSS('./test.css');
 // parseCSS('/Users/eholladay/Documents/Projects/100-javascript-projects/js-learning/exercises/ex-6-css-parsing/test.css');
 
 
