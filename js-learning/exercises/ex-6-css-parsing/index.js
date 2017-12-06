842266d030403bad0f0570cdc9cbfe0829035dda
Elyse Holladay Cherewaty <eholladay@indeed.com>
2017-12-06 15:49:10 -0800

    add notes

--- js-learning/exercises/ex-6-css-parsing/index.js
+++ js-learning/exercises/ex-6-css-parsing/index.js
@@ -35,15 +35,27 @@ const parseCSS = function(path) {
         // because data isn't yet a css file?
         var cssdata = data.toString();
         // console.log(cssdata);
-        var result = css.parse(cssdata, { source: './test.css' });
+        var result = css.parse(cssdata, { source: path });
         console.log(result); // there we go
+        // now we have to sort thru these and find declaration types
+        // and then of those which ones start with a dash
+        // object, stylesheet obj, rules array, object inside that array with some number value
+        // (obj 9 in this case is <html> and has a prefixed selector)
+        // object[9].declarations.object[n].property = starts with a dash "-webkit-text-size-adjust"
+        // object[9].type = rule
+        // object[9].selectors[0] = html
+        // the type=rule inside this object we want to reduce and get only the objects that are rules
+        // then for the objs that are rules, find the declarations obj, property value
+        // if the prop value starts with a dash
+        // then reduce on that and output an object with the selector
+        
     });
 }
 
 // TODO
 // "full path to a CSS file" - is there a way to enforce or require absolute paths, or does a relative path count?
-parseCSS('./test.css');
-// parseCSS('/Users/eholladay/Documents/Projects/100-javascript-projects/js-learning/exercises/ex-6-css-parsing/test.css');
+// parseCSS('./test.css');
+parseCSS('/Users/eholladay/Documents/Projects/100-javascript-projects/js-learning/exercises/ex-6-css-parsing/test.css');
 
 
 // var obj = css.parse('body { font-size: 12px; }');
