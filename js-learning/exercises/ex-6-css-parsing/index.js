4f2315236134007fcdf2b6b5575f999030098959
Elyse Holladay Cherewaty <eholladay@indeed.com>
2017-12-07 16:19:35 -0800

    filter on vendor props

--- js-learning/exercises/ex-6-css-parsing/index.js
+++ js-learning/exercises/ex-6-css-parsing/index.js
@@ -7,66 +7,45 @@ var css = require('css');
 // Use the rework/css CSS parser to generate an object that represents the contents of the CSS file, which you can then traverse to identify places where vendor prefixes are used.
 
 
-const parseCSS = function(path) {
+const parseCSS = function(path, callback) {
     fs.readFile(path, function (err, data) {
         if (err) {
-            return console.error(err);
+            callback(err); // second param (the data) is automatically undefined if not passed
+            return; // don't need this for the callback
         }
-        // console.log(data.toString());
-        // create here an array of rules that use vendor prefixes
-        // so we need to pass `data` to the rework css parser
-        // and then find properties that start with a dash?
 
-        // this fails with
-        // return this.mapVisit(node.stylesheet.rules, '\n\n');
-        // TypeError: Cannot read property 'rules' of undefined
-        // var result = css.stringify(data);
-        
-        
-        
-        
-        // these two lines as written fail with
-        //     if ('/' != css.charAt(0) || '*' != css.charAt(1)) return;
-        // TypeError: css.charAt is not a function
-        // var obj = css.parse(data);
-        // console.log(css.stringify(obj));
-        // as does this one:
-        // var result = css.parse(data, { source: './test.css' });
-        // because data isn't yet a css file?
-        var cssdata = data.toString();
-        // console.log(cssdata);
+        var cssdata = data.toString(); // because data isn't yet a css
         var parsedcss = css.parse(cssdata, { source: path });
-        // console.log(parsedcss); // there we go
-        // now we have to sort thru these and find declaration types
-        // and then of those which ones start with a dash
-        // object, stylesheet obj, rules array, object inside that array with some number value
-        // (obj 9 in this case is <html> and has a prefixed selector)
-        // object[9].declarations.object[n].property = starts with a dash "-webkit-text-size-adjust"
-        // object[9].type = rule
-        // object[9].selectors[0] = html
-        // the type=rule inside this object we want to filter and get only the objects that are rules
-        // then for the objs that are rules, find the declarations obj, property value
-        // if the prop value starts with a dash
-        // then filter on that and output an object with the selector
-
+        
         var allrules = parsedcss.stylesheet.rules;
-        var ruletypeonly = allrules.filter((type) => type == 'rule');
-        // why does this return an empty array?
+        var ruletypeonly = allrules.filter((rule) => rule.type == 'rule');
+        // console.log(ruletypeonly);
 
-        // console.log( allrules );
-        console.log(ruletypeonly);
+        var foo = [];
 
+        ruletypeonly.forEach((rule) => {
+            rule.declarations.forEach((declaration) => {
+                if (declaration.property.charAt(0) == '-') {
+                    foo.push(rule.selectors);
+                }
+            })
+            // console.log(foo);
+        });
+
+        // deduplicate
+        var selectors = foo.filter(function (elem, pos) {
+            return foo.indexOf(elem) == pos;
+        });
+
+        console.log(selectors);
+
+        // callback(undefined, whateverthedataarrayis);
+        // here, we have to pass the error as undefined, since the data is the second param, and callback takes two args
     });
+
 }
 
-// TODO
-// "full path to a CSS file" - is there a way to enforce or require absolute paths, or does a relative path count?
-// parseCSS('./test.css');
 parseCSS('/Users/eholladay/Documents/Projects/100-javascript-projects/js-learning/exercises/ex-6-css-parsing/test.css');
-
-
-// var obj = css.parse('body { font-size: 12px; }');
-// console.log( css.stringify(obj) );
 
 
 module.exports = parseCSS;
